e4b5b5a496e6d819df150ba4428f804d361621f6
gloriagrzy <gloria.grzybowski@gmail.com>
2025-09-12 16:33:49 -0500

    updates

--- shoppingCartOOP/src/test/java/org/example/service/ItemTest.java
+++ shoppingCartOOP/src/test/java/org/example/service/ItemTest.java
@@ -1,4 +1,48 @@
 package org.example.service;
 
+import org.example.model.Item;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ItemTest {
+    //Arrange
+    Item itemTest = new Item("testName", 4.00);
+
+    @Test
+    public void getPriceTest() {
+        Double expected = 4.00;
+        //ACT
+        Double actual = itemTest.getPrice();
+        //ASSERT
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setPriceTest() {
+        Double expected = 6.00;
+        //ACT
+        itemTest.setPrice(6.00);
+        //ASSERT
+        assertEquals(expected, itemTest.getPrice());
+
+    }
+
+    @Test
+    public void getNameTest() {
+        String expected = "testName";
+        //ACT
+        String actual = itemTest.getName();
+        //ASSERT
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNameTest() {
+        String expected = "goodTestName";
+        //ACT
+        itemTest.setName("goodTestName");
+        //ASSERT
+        assertEquals(expected, itemTest.getName());
+    }
 }
