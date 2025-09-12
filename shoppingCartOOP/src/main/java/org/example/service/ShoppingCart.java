f4ecf837af0db1b2ff7246e32daf15121f52ce8f
Brian Pennington <44206137+brian-d-pennington@users.noreply.github.com>
2025-09-12 11:32:28 -0500

    got boilerplate into Shopping Cart

--- shoppingCartOOP/src/main/java/org/example/service/ShoppingCart.java
+++ shoppingCartOOP/src/main/java/org/example/service/ShoppingCart.java
@@ -1,4 +1,13 @@
 package org.example.service;
 
+import org.example.model.Customer;
+import org.example.model.Item;
+
+import java.util.List;
+import java.util.Map;
+
 public interface ShoppingCart {
+    public List<Item> getCustomerItems(Customer customer);
+    public Map<Customer,List<Item>> customerAddItem(Customer customer, Item item);
+    public Map<Customer,List<Item>> customerRemoveItem(Customer customer, Item item);
 }
--- shoppingCartOOP/src/main/java/org/example/service/ShoppingCartImpl.java
+++ shoppingCartOOP/src/main/java/org/example/service/ShoppingCartImpl.java
@@ -1,4 +1,38 @@
 package org.example.service;
 
+import org.example.model.Customer;
+import org.example.model.Item;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ShoppingCartImpl implements ShoppingCart {
+    private Map<Customer,List<Item>> shoppingCart;
+    private Customer customer;
+    private List<Item> customerItems;
+
+    public ShoppingCartImpl(Customer customer, List<Item> customerItems) {
+        this.customer = customer;
+        this.customerItems = customerItems;
+        shoppingCart = new HashMap<>();
+    }
+
+    public List<Item> getCustomerItems(Customer customer) {
+        return shoppingCart.get(customer);
+    }
+
+    public Map<Customer,List<Item>> customerAddItem(Customer customer, Item item) {
+        List<Item> customerItems = shoppingCart.get(customer);
+        customerItems.add(item);
+        shoppingCart.put(customer, customerItems);
+        return shoppingCart;
+    }
+
+    public Map<Customer,List<Item>> customerRemoveItem(Customer customer, Item item) {
+        List<Item> customerItems = shoppingCart.get(customer);
+        customerItems.remove(item);
+        shoppingCart.put(customer, customerItems);
+        return shoppingCart;
+    }
 }
