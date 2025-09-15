342005dec3ea37d488d61d1fbe07bbd923528668
Brian Pennington <44206137+brian-d-pennington@users.noreply.github.com>
2025-09-15 11:12:39 -0500

    MenuControllerTests around Add Funds

--- shoppingCartOOP/src/test/java/org/example/service/MenuControllerTest.java
+++ shoppingCartOOP/src/test/java/org/example/service/MenuControllerTest.java
@@ -1,16 +1,43 @@
 package org.example.service;
 
+import org.example.model.Customer;
+import org.example.utils.Util;
+import org.example.view.ConsoleUI;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MenuControllerTest {
+    Customer customerToTest;
+    Util util;
 
     @BeforeEach
     void setUp() {
-
+         customerToTest = new Customer("Test User", 10.00);
+         util = new Util();
     }
 
     @Test
-    void caseOneTest() {
+    void testCustomerAddsFunds() {
+        // arrange
+        double expected = 15.00;
+        // act
+        customerToTest.customerAddsFunds(5.00);
+        double actual = util.roundMoneyTwoDecimals(customerToTest.getFunds());
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void TestCustomerAddsToNegativeFunds() {
+        // arrange
+        customerToTest.setFunds(-10.00);
+        double expected = 5.00;
+        // act
+        customerToTest.customerAddsFunds(15.00);
+        double actual = util.roundMoneyTwoDecimals(customerToTest.getFunds());
+        // assert
+        assertEquals(expected, actual);
     }
 }
