ef67abce4c7e94cd0ede3d9afe01027473b2b350
Brian Pennington <44206137+brian-d-pennington@users.noreply.github.com>
2025-09-15 13:51:40 -0500

    setup for ConsoleUITest cases

--- shoppingCartOOP/src/test/java/org/example/view/ConsoleUITest.java
+++ shoppingCartOOP/src/test/java/org/example/view/ConsoleUITest.java
@@ -1,7 +1,20 @@
 package org.example.view;
 
+import org.example.model.Customer;
+import org.example.utils.Util;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConsoleUITest {
+    ConsoleUI ui;
+    Util util;
+    Customer customer;
 
+    @BeforeEach
+    void setUp() {
+        ui = new ConsoleUI();
+        util = new Util();
+        customer = new Customer("Test User", 10.00);
+    }
 }
