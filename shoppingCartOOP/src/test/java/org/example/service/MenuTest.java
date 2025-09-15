d15be237facd9a508759eb2c961ae026e7019e96
Brian Pennington <44206137+brian-d-pennington@users.noreply.github.com>
2025-09-15 13:18:12 -0500

    MenuTests created

---
+++ shoppingCartOOP/src/test/java/org/example/service/MenuTest.java
@@ -0,0 +1,52 @@
+package org.example.service;
+
+import org.example.model.Item;
+import org.example.view.Menu;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MenuTest {
+    Menu testMenu;
+    List<Item> itemListToTest;
+
+    @BeforeEach
+    void setUp() {
+        testMenu = new Menu();
+    }
+
+    @Test
+    void testDisplayMenuNotNull() {
+        // arrange
+
+        // act
+        List<Item> menuTest = testMenu.createMenu();
+        // assert
+        assertNotNull(menuTest);
+    }
+
+    @Test
+    void testCreateMenuReturnsCorrectLength() {
+        // arrange
+        int expected = 8;
+        // act
+        List<Item> menuTest = testMenu.createMenu();
+        // assert
+        assertEquals(expected, menuTest.size());
+    }
+
+    @Test
+    void testFirstIndexValueOfReturnedList() {
+        // arrange
+        Item testItem = new Item("Hershey Bar", 2.00);
+        String expected = testItem.getName();
+        // act
+        List<Item> menuTest = testMenu.createMenu();
+        // assert
+        assertEquals(expected, menuTest.get(0).getName());
+    }
+}
